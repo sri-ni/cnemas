4ec2ec48eaf48f9fa7a7acd5e34f70271a694781
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-09-03 23:05:00 -0500

    Popularity sort functionality added.

--- app/scripts/services/moviespopular.js
+++ app/scripts/services/moviespopular.js
@@ -11,14 +11,13 @@ angular.module('cnemasApp')
   .service('moviesPopular', ['$http', '$q', function ($http, $q) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    this.getPopularMovies = function(page) {
+    this.getPopularMovies = function(page, popularity) {
       var deferred = $q.defer();
 
-      // movie api
-      //http://api.themoviedb.org/3/movie/popular?api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page=
-      // discover api
-      //http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page=
-      $http.get('http://api.themoviedb.org/3/movie/popular?api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page='+page).then(function(result){
+      console.log('getPopularMovies popularity: ', popularity);
+
+      $http.get('http://api.themoviedb.org/3/discover/movie?sort_by=popularity.'+popularity+'&api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page='+page)
+        .then(function(result){
         deferred.resolve(result.data);
         console.log('deferred.resolve: ', result.data);
       });
@@ -26,3 +25,9 @@ angular.module('cnemasApp')
       return deferred.promise;
     };
   }]);
+
+
+  // movie api
+  //http://api.themoviedb.org/3/movie/popular?api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page=
+  // discover api
+  //http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2cdc6bb4ff1cc8f902ecb3a7101cc992&page=
